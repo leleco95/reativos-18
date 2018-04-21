4628ba9da2107ac0d2577db7bdc99dacb792cb2f
thiagola92 <thiagola92@gmail.com>
2018-04-21 12:16:58 -0300

    Apenas mudando o modo de escrever

--- trabalho 02/enemy.lua
+++ trabalho 02/enemy.lua
@@ -1,28 +1,29 @@
 local enemy = {}
 
 function enemy.newEnemy(category)
+
+  local function update(self)
+    while true do
+      self.y = self.y + 2
+      if self.y > 600 then
+        self.y = 0
+      end
+      coroutine.yield()
+    end
+  end
+
+  local function draw(self)
+    love.graphics.setColor(0, 0, 255)
+    love.graphics.circle("fill", self.x, self.y, 10)
+    love.graphics.setColor(255, 255, 255)
+  end
+
   return {
     x = 425,
     y = 0,
     category = category,
-    update =
-      coroutine.wrap(
-        function(self)
-          while true do
-            self.y = self.y + 2
-            if self.y > 600 then
-              self.y = 0
-            end
-            coroutine.yield()
-          end
-        end
-      ),
-    draw =
-      function (self)
-        love.graphics.setColor(0, 0, 255)
-        love.graphics.circle("fill", self.x, self.y, 10)
-        love.graphics.setColor(255, 255, 255)
-      end,
+    update = coroutine.wrap(update),
+    draw = draw,
   }
 end
 
--- trabalho 02/tower.lua
+++ trabalho 02/tower.lua
@@ -2,23 +2,26 @@ local tower = {}
 require "general"
 
 function tower.newTower(x, y, category)
+
+  local function fire(self)
+    while true do
+      if hit(x, y, category) then
+        wait(category/2, self)
+      else
+        wait(0, self)
+      end
+    end
+  end
+
+  local function draw()
+    love.graphics.setColor(255, 0, 0)
+    love.graphics.circle("fill", x, y, 10)
+    love.graphics.setColor(255, 255, 255)
+  end
+
   return {
-    fire =
-      function(self)
-        while true do
-          if hit(x, y, category) then
-            wait(category/2, self)
-          else
-            wait(0, self)
-          end
-        end
-      end,
-    draw =
-      function()
-        love.graphics.setColor(255, 0, 0)
-        love.graphics.circle("fill", x, y, 10)
-        love.graphics.setColor(255, 255, 255)
-      end,
+    fire = fire,
+    draw = draw,
   }
 end
 
